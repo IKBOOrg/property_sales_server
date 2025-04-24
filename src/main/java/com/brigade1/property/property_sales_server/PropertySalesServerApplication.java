22934532f8b813b22ab2c9fdd6ba7624daadc172
Lisa <elbondarenko04@gmail.com>
2025-04-24 13:40:45 +0300

    first commit

---
+++ src/main/java/com/brigade1/property/property_sales_server/PropertySalesServerApplication.java
@@ -0,0 +1,13 @@
+package com.brigade1.property.property_sales_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertySalesServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertySalesServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brigade1/property/property_sales_server/PropertySalesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brigade1.property.property_sales_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertySalesServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
